4c8294ea30639b4098d8b7f611ee248bfb654c93
rramiro-dev <reche.ramiro98@gmail.com>
2024-05-21 19:34:17 -0300

    Este archivo es una refactorizacion de codigo y logica de programacion del archivo desafio3.py. Esta parcialmente finalizado, estamos aguardando la confirmacion de un profesor con respecto a un punto para poder hacerlo y ya quedaria finalizado el desafio.

--- desafio3.py
+++ desafio3.py
@@ -10,8 +10,32 @@ def ingreso_datos(mensaje, tipo_dato=int):
             opcion = tipo_dato(input(mensaje))
             flag = False
         except ValueError:
-            print(f'El tipo de dato es incorrecto, por favor ingrese un valor de tipo {tipo_dato}')
+            print(f'El tipo de dato es incorrecto, por favor ingrese un valor de tipo {[tipo_dato]}')
     return opcion
+"""
+def patear_penal():
+    '''
+        donde patear penal
+    '''
+    _jugador = ingreso_datos('Elegi donde patear [1-9]: ')
+    return _jugador
+
+def atajar_penal(_arco):
+    '''
+        donde atajar penal
+    '''
+    _jugador = random.randint(1,len(_arco))
+    return _jugador
+
+def ataja_si_o_si():
+    pass
+
+def tarea():
+    pass
+
+def tarea():
+    pass
+"""
 
 # Variables
 ARCO = (
@@ -25,7 +49,7 @@ marcador = {
     'pc': [0]*5 # [0, 0, 0, 0, 0]
 }
 
-tanda_desempate = list() # [0, 0]
+tanda_desempate = ['', ''] # [0, 0]
 
 indice_tiros = 0
 for i in range(10): # 0 - 9
@@ -44,7 +68,6 @@ for i in range(10): # 0 - 9
         pc = random.randint(1,len(ARCO))
     
     # Otorgamos los puntos en el marcador
-
     # Validamos si el usuario/pc patean/atajan en cualquiera de las posiciones 2, 5, y/u 8
     if usuario in (2,5,8) and pc in (2,5,8):
         if i % 2 == 0: # Usuario patea
@@ -78,9 +101,15 @@ for i in range(10): # 0 - 9
 print(marcador['usuario'])
 print(marcador['pc'])
 
+resultado_temp = list()
+for puntaje in marcador.values():
+    resultado = puntaje.count('Y')
+    resultado_temp.append(resultado)
+print(f'puntaje: {resultado_temp}')
 
+resultado_temp = [3,3]
 # Rondas para definir un empate de penales
-if marcador['usuario'] == marcador['pc']:
+if resultado_temp[0] == resultado_temp[1]:
     flag = True
     indice_tiros = 0
     while flag:
@@ -90,8 +119,10 @@ if marcador['usuario'] == marcador['pc']:
         # Patea usuario
         usuario = ingreso_datos('Elegi donde patear [1-9]: ' if not indice_tiros % 2 == 0 else 'Elegi donde atajar [1-9]: ')
         pc = random.randint(1,len(ARCO))
-        
-        
+        print(f'Ronda: {indice_tiros}', 
+          f'Usuario patea: {usuario}' if not i % 2 == 0 else f'Usuario ataja: {usuario}', 
+          f'PC Ataja: {pc}' if not i % 2 == 0 else f'PC patea: {pc}', 
+          f'Gana punto: {punto}', sep='\n')
         # Validamos si el usuario/pc patean/atajan en cualquiera de las posiciones 2, 5, y/u 8
         if usuario in (2,5,8) and pc in (2,5,8):
             if not i % 2 == 0: # Usuario patea
@@ -116,12 +147,14 @@ if marcador['usuario'] == marcador['pc']:
                     tanda_desempate[pos] = 'Y'
                     punto = 'usuario'
         
-        if tanda_desempate[0] == tanda_desempate[1]:
-            if tanda_desempate[0] == 'Y':
-                ganador = 'usuario'
-            else:
-                ganador = 'pc'
-
+        if tanda_desempate[0] == tanda_desempate[1] and tanda_desempate[0] == 'Y':
+            ganador = 'usuario'
             flag = False
+        else:
+            ganador = 'pc'
+            flag = False
+else:
+    # Define ganador en la primera tanda de penales
+    ganador = 'Ramiro'
 
 print(ganador)
---
+++ desafio_3.py
@@ -0,0 +1,146 @@
+'''
+Copa algoritmia y programacion 2024
+Desafio: 3
+
+Tema: Penales entre Argentina (Usuario) y Paises Bajos (PC)
+'''
+# Modulos
+import random
+
+# Funciones
+def ingreso_datos(mensaje: str, tipo_dato=int):
+    '''
+    Valida el ingreso de datos, segun el tipo de dato. Por defecto, es int.
+    '''
+    flag: bool = True
+    while flag:
+        try:
+            dato: tipo_dato = tipo_dato(input(mensaje))
+            flag: bool = False
+        except ValueError:
+            print(f'El tipo de dato ingresado es incorrecto. Por favor intente nuevamente...')
+    return dato
+
+def es_par(n: int):
+    '''
+    Devuelve True si el numero n es par, de lo contrario, devuelve False.
+    '''
+    return n % 2 == 0
+
+def eleccion_usuario(_ronda: int, corte_inicial: int, corte_final: int):
+    '''
+    Valida que el ingreso del dato este en el rango numerico correspondiente, segun los valores pasados como argumento,
+    y luego devuelve el valor ingresado.
+    '''
+    flag: bool = True
+    while flag:
+        objetivo: int = ingreso_datos(f'Elegi donde {'patear' if es_par(_ronda) else 'atajar'} ({corte_inicial}-{corte_final}): ')
+        if (objetivo >= corte_inicial and objetivo <= corte_final):
+            flag: bool = False
+        else:
+            print(f'El numero ingresado es incorrecto, intente nuevamente...')
+    return objetivo
+
+def eleccion_pc(_arco):
+    '''
+    Devuelve un valor al azar utilizando como base la lista pasada como argumento.
+    '''
+    return random.choice(_arco)
+
+def es_gol(_eleccion_pateador: int, _eleccion_arquero: int):
+    '''
+    Valida y devuelve True si el pateador convierte un gol. Las condiciones de conversion de gol son:
+    - la eleccion de ambos jugadores NO debe ser cualquiera de estas en la misma jugada: 2, 5, 8
+    - Si no se cumple la anterior, la eleccion de ambos jugadores debe ser distinta
+    '''
+    if _eleccion_pateador in (2, 5, 8) and _eleccion_arquero in (2, 5, 8):
+        return False # Atajada
+    elif _eleccion_pateador != _eleccion_arquero:
+        return True # Gol
+    else:
+        return False # Atajada
+
+def imprimir_marcador(_ronda, _equipo1, _equipo2, _tiro_equipo1, _tiro_equipo2, _resultado):
+    '''
+    Imprime por terminal el estado del marcador
+    '''
+    print(
+        f'{'-'*40}',
+        f'Ronda: {_ronda}',
+        f'{'-'*7}',
+        f'Marcador: \n\t{
+            f'Argentina: {_equipo1}'
+        }\n\t{
+            f'Paises bajos: {_equipo2}'
+        }',
+        f'{'-'*7}',
+        f'Coordenadas de tiro: \n\t{
+            f'Argentina: {_tiro_equipo1}'
+        }\n\t{
+            f'Paises Bajos: {_tiro_equipo2}'
+        }',
+        f'{'-'*7}',
+        f'Resultado: {_resultado}',
+        f'{'-'*40}',
+        sep='\n'
+    )
+
+def se_puede_finalizar_adelantado(_lista_jugador_1: list, _lista_jugador_2: list, _finalizar_juego: bool=False):
+    '''
+    Evalua y retorna True en el caso que los tiros aún pendientes de ambos jugadores no alcancen para un posible empate o dar vuelta el partido,
+    caso contrario, devuelve False.
+
+    Ejemplo:
+        - Jugador 1 ['N', 'N', 'N', '', '']
+        - Jugador 2 ['Y', 'Y', 'Y', '', '']\n
+    Resultado: devuelve False porque Jugador 1 no llega a empatar el partido.
+    '''
+    cant_goles_jugador_1: int = _lista_jugador_1.count('Y')
+    cant_goles_jugador_2: int = _lista_jugador_2.count('Y')
+    tiros_pendientes: int = _lista_jugador_1.count('') # Tambien se puede usar _lista_jugador_2, ya que tienen el mismo len().
+    goles_jugador_menos_goles: int = cant_goles_jugador_1 if cant_goles_jugador_1 < cant_goles_jugador_2 else cant_goles_jugador_2
+    goles_jugador_mas_goles: int = cant_goles_jugador_1 if cant_goles_jugador_1 > cant_goles_jugador_2 else cant_goles_jugador_2
+    
+    if (tiros_pendientes + goles_jugador_menos_goles) < goles_jugador_mas_goles:
+        return True
+    else:
+        return _finalizar_juego
+
+
+# Variables
+ARCO: tuple = (
+    1, 2, 3,
+    4, 5, 6,
+    7, 8, 9
+)
+argentina: list = [''] * 5
+paises_bajos: list = [''] * 5
+
+ejecutar_juego: bool = True # Variable que controla la ejecucion del juego
+ronda: int = 0 # Variable acumuladora para contar las rondas de penales que van sucediendo 
+
+# Ejecucion de la tanda de penales
+for indice in range(len(argentina) + len(paises_bajos)): # 0 - 9
+    if es_par(indice):
+        ronda += 1
+    
+    # Pedido de coordenadas de tiro a cada equipo
+    jugadora_argentina: int = eleccion_usuario(indice, ARCO[0], ARCO[-1])
+    jugadora_paisesbajos: int = eleccion_pc(ARCO)
+
+    # Anotar puntos
+    if es_par(indice):
+        # En indices pares patea Argentina
+        argentina[ronda - 1] = 'Y' if es_gol(jugadora_argentina, jugadora_paisesbajos) else 'N'
+        resultado = 'Gol' if argentina[ronda - 1] == 'Y' else 'Errado'
+    else:
+        # En indices impares patea Paises Bajos
+        paises_bajos[ronda - 1] = 'Y' if es_gol(jugadora_paisesbajos, jugadora_argentina) else 'N'
+        resultado = 'Gol' if paises_bajos[ronda - 1] == 'Y' else 'Errado'
+    
+    # Imprimimos el estado actual del marcador
+    imprimir_marcador(ronda, argentina, paises_bajos, jugadora_argentina, jugadora_paisesbajos, resultado)
+
+    # Evaluamos si es necesario seguir jugando rondas, o podemos finalizar de forma anticipada
+    if se_puede_finalizar_adelantado():
+        break
